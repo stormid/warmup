06c26524381aea9ec40406c92188be6137082db1
James Lynch <james@stormid.com>
2010-07-29 21:31:52 +0100

    Fix for applying default template name if no template is specified as a parameter

--- warmup/Git.cs
+++ warmup/Git.cs
@@ -47,7 +47,7 @@ namespace warmup
                 Console.WriteLine(output);
                 Console.WriteLine(error);
 
-                var templateName = piecesOfPath[1];
+                var templateName = piecesOfPath.Length > 1 ? piecesOfPath[1] : WarmupConfiguration.settings.DefaultTemplate;
                 GitTemplateExtractor extractor = new GitTemplateExtractor(target, templateName);
                 extractor.Extract();
                 //string git_directory = Path.Combine(target.FullPath, ".git");
